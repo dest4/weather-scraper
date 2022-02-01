41d0b888ad7fff7a664447117015fc2831383287
Alexandre Storelli <a_github@storelli.fr>
2022-02-01 23:29:16 +0100

    Failsafe scenario if failing API

--- scraper.js
+++ scraper.js
@@ -1,6 +1,11 @@
 const axios = require('axios');
+const assert = require('assert');
 
 const VERBOSE = process.argv.includes('--verbose');
+
+const CHECKWX_KEY = 'a61f8e9f3c9a4f8083910ef199';
+const LAT = 48.841348;
+const LON = 2.386885;
 
 const accentsMap = new Map([
     ["A", "Á|À|Ã|Â|Ä"],
@@ -25,19 +30,24 @@ const removeAccents = (text) =>
 (async function () {
     try {
         // get cookie
-        const response = await axios.head('https://meteofrance.com/previsions-meteo-france/paris-12e-arrondissement/75012');
-        const cookie = response.headers['set-cookie'][0];
+        let derivedToken = '';
+        try {
+            const response = await axios.head('https://meteofrance.com/previsions-meteo-france/paris-12e-arrondissement/75012');
+            const cookie = response.headers['set-cookie'][0];
 
-        // build token from cookie
-        const cookieContents = cookie.match(new RegExp('(^| )mfsession=([^;]+)'));
-        if (!cookieContents) {
-            throw new Error('cookie not recognized');
+            // build token from cookie
+            const cookieContents = cookie.match(new RegExp('(^| )mfsession=([^;]+)'));
+            if (!cookieContents) {
+                throw new Error('cookie not recognized');
+            }
+            const cookieField = decodeURIComponent(cookieContents[2]);
+            derivedToken = cookieField.replace(/[a-zA-Z]/g, function (e) {
+                var t = e <= 'Z' ? 65 : 97;
+                return String.fromCharCode(t + (e.charCodeAt(0) - t + 13) % 26)
+            });
+        } catch (e) {
+            console.warn(`could not get token ${e}`);
         }
-        const cookieField = decodeURIComponent(cookieContents[2]);
-        const derivedToken = cookieField.replace(/[a-zA-Z]/g, function (e) {
-            var t = e <= 'Z' ? 65 : 97;
-            return String.fromCharCode(t + (e.charCodeAt(0) - t + 13) % 26)
-        });
 
         const headers = {
             'credentials': 'include',
@@ -58,61 +68,83 @@ const removeAccents = (text) =>
         };
 
         // get actual forecast data
-        const forecast = await axios.get('https://rpcache-aa.meteofrance.com/internet2018client/2.0/forecast?lat=48.841348&lon=2.386885&id=&instants=&day=2', headers);
-        const daily = forecast.data.properties['daily_forecast'][new Date().getHours() >= 18 ? 1 : 0];
-        if (VERBOSE) {
-            console.log('FORECAST');
-            console.log(forecast.data);
-            console.log(forecast.data.properties['daily_forecast']);
-            console.log(forecast.data.properties['forecast']);
+        let daily = { time: '', 'daily_weather_description': '??', 'T_min': '99', 'T_max': '99' };
+        try {
+            assert(derivedToken, 'no token');
+            const forecast = await axios.get(`https://rpcache-aa.meteofrance.com/internet2018client/2.0/forecast?lat=${LAT}&lon=${LON}&id=&instants=&day=2`, headers);
+            daily = forecast.data.properties['daily_forecast'][new Date().getHours() >= 18 ? 1 : 0];
+            if (VERBOSE) {
+                console.log('FORECAST');
+                console.log(forecast.data);
+                console.log(forecast.data.properties['daily_forecast']);
+                console.log(forecast.data.properties['forecast']);
+            }
+        } catch (e) {
+            console.warn(`cannot fetch daily forecast ${e}`);
         }
 
         // get raining forecast
-        const raining = await axios.get('https://rpcache-aa.meteofrance.com/internet2018client/2.0/nowcast/rain?lat=48.841348&lon=2.386885', headers);
-        const rainIntensity = Math.max(...raining.data.properties.forecast.map(fc => fc['rain_intensity']));
-        let rainLabel = '  ';
-        if (rainIntensity >= 4) {
-            rainLabel = 'RA';
-        } else if (rainIntensity >= 2) {
-            rainLabel = 'ra'
-        }
+        let rainLabel = '??';
+        try {
+            assert(derivedToken, 'no token');
+            const raining = await axios.get(`https://rpcache-aa.meteofrance.com/internet2018client/2.0/nowcast/rain?lat=${LAT}&lon=${LON}`, headers);
+            const rainIntensity = Math.max(...raining.data.properties.forecast.map(fc => fc['rain_intensity']));
+            rainLabel = '  ';
+            if (rainIntensity >= 4) {
+                rainLabel = 'RA';
+            } else if (rainIntensity >= 2) {
+                rainLabel = 'ra'
+            }
 
-        if (VERBOSE) {
-            console.log('RAINING');
-            console.log(raining.data);
-            console.log(raining.data.properties.forecast);
+            if (VERBOSE) {
+                console.log('RAINING');
+                console.log(raining.data);
+                console.log(raining.data.properties.forecast);
+            }
+            // const rainForecast = raining.data.properties.forecast.map(fc => fc['rain_intensity']).join('');
+            // console.log(rainForecast);
+        } catch (e) {
+            console.warn(`cannot fetch raining forecast ${e}`);
         }
-        // const rainForecast = raining.data.properties.forecast.map(fc => fc['rain_intensity']).join('');
-        // console.log(rainForecast);
 
         const l1 = `${daily.time.slice(8, 10)}-${daily.time.slice(5, 7)} ${Math.round(daily['T_min'])}->${Math.round(daily['T_max'])}C ${rainLabel}          `.slice(0, 16);
         const l2 = `${removeAccents(daily['daily_weather_description'])}                `.slice(0, 16);
 
-        const ephemeris = await axios.get('https://rpcache-aa.meteofrance.com/internet2018client/2.0/ephemeris?lat=48.841348&lon=2.386885', headers)
-        const ephData = ephemeris.data.properties.ephemeris;
-        // const sunrise = daily['sunrise_time'].slice(11, 16);
-        // const sunset = daily['sunset_time'].slice(11, 16);
-        
-        if (VERBOSE) {
-            console.log('EPHEMERIS');
-            console.log(ephemeris.data);
+        let saintPrefix = '?', saintName = '';
+        try {
+            assert(derivedToken, 'no token');
+            const ephemeris = await axios.get(`https://rpcache-aa.meteofrance.com/internet2018client/2.0/ephemeris?lat=${LAT}&lon=${LON}`, headers)
+            const ephData = ephemeris.data.properties.ephemeris;
+            // const sunrise = daily['sunrise_time'].slice(11, 16);
+            // const sunset = daily['sunset_time'].slice(11, 16);
+
+            if (VERBOSE) {
+                console.log('EPHEMERIS');
+                console.log(ephemeris.data);
+            }
+
+            const saintSplit = ephData.saint.split(' ');
+            saintPrefix = saintSplit[0] === 'Saint' ? 'St' : 'Ste';
+            saintName = saintSplit.slice(1).join(' ');
+        } catch (e) {
+            console.warn(`could not get ephemeris ${e}`);
         }
-
-        const metar = await axios.get('https://api.checkwx.com/metar/LFPO', { 'headers': {'X-API-Key': 'a61f8e9f3c9a4f8083910ef199'}});
-        const qnh = metar.data.data[0].split(' ').find(block => block[0] === 'Q').slice(1);
-        const wind = metar.data.data[0].split(' ').find(block => block.slice(-2) === 'KT');
-
-        if (VERBOSE) {
-            console.log('METAR');
-            console.log(metar.data);
-        }
-
-        const l3 = `${qnh} ${wind}                `.slice(0, 16);
-        
-        const saintSplit = ephData.saint.split(' ');
-        const saintPrefix = saintSplit[0] === 'Saint' ? 'St' : 'Ste';
-        const saintName = saintSplit.slice(1).join(' ');
         const l4 = `${saintPrefix} ${removeAccents(saintName)}                  `.slice(0, 16);
+
+        let qnh = '?', wind = '';
+        try {
+            const metar = await axios.get('https://api.checkwx.com/metar/LFPO', { 'headers': {'X-API-Key': CHECKWX_KEY}});
+            qnh = metar.data.data[0].split(' ').find(block => block[0] === 'Q').slice(1);
+            wind = metar.data.data[0].split(' ').find(block => block.slice(-2) === 'KT');
+
+            if (VERBOSE) {
+                console.log('METAR');
+                console.log(metar.data);
+            }
+        } catch (e) {
+            console.warn(`could not fetch metar ${e}`);
+        }
+        const l3 = `${qnh} ${wind}                `.slice(0, 16);
 
         const result = `${l1}${l3}\n${l2}${l4}`;
         console.log(result);
